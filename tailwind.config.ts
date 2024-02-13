acd356aa4be5c0b8fb484b5941232b9888d2afc7
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-12 19:46:56 -0500

    fix screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      'sm': {'min': '240px'},
+
+      'md': {'min': '768px'},
+
+      'lg': {'min': '1024px'},
+
+      'xl': {'min': '1280px'},
+
+      '2xl': {'min': '1536px'},
+    },
   },
   plugins: [],
 };
