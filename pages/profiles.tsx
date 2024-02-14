091244d60181a8277e0c6ca8b9ea253ae5fc911b
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-14 18:26:04 -0500

    Add ui for profiles page

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,6 @@
+import useCurrentUser from "@/hook/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -22,9 +22,31 @@ export async function getServerSideProps(context: NextPageContext) {
 
 
 const Profiles = ()=>{
+  const {data: user} = useCurrentUser();
+
+
   return <>
-  <p className="text-white text-4xl">Profiles</p>
-  </>
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is Watching?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={()=>{}}>
+            <div className="group flex-row w-44 mx-auto">
+              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                <img src="/images/default-blue.png" alt="" />
+              </div>
+
+              <div className="mt-4 text-gray-400 text2xl text-center group-hover:text-white">
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
 }
 
 
