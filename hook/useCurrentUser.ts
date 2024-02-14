e48e853b385399b9db369f3d56ddfa6730a1bdf7
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-14 17:42:34 -0500

    COMPLETED SIGNIIN LOGIC!!!!!! Lesssss gooo

--- hook/useCurrentUser.ts
+++ hook/useCurrentUser.ts
@@ -6,6 +6,7 @@ import useSWR from "swr";
 
 const useCurrentUser =()=>{
   const {data, error,isLoading, mutate}= useSWR('/api/current', fetcher)
+  
   return {data, error,isLoading, mutate}
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import useCurrentUser from "@/hook/useCurrentUser";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if (!session){
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      }
+    }
+  }
+
+  return {props:{}}
+}
+
 export default function Home() {
+  const {data: user} = useCurrentUser();
+
+  
   return (
     <>
+    <h1>VOOODJD</h1>
+    <p className="text-white">Logged in as: {user?.email}</p>
+    <button className="h-10 bg-white" onClick={()=>signOut()}>Logout</button>
     </>
   );
 }
