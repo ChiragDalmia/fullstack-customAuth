8abc25d8819de8694dd7ba3d53b23f2d307e96fc
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-14 01:42:52 -0500

    fix fetcher return type

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 
-const fetcher = (url: string)=>{
-  axios.get(url).then((res)=>res.data);
-}
+const fetcher = (url:string) => fetch(url).then(res => res.json())
 
 export default fetcher;
