5f15e6bd513046b3252b7711ab0bc9cfb6953760
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-16 14:54:22 -0500

    Refactored InfoModal component readability and consistency.

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
 import PlayButton from '@/components/PlayButton';
@@ -8,45 +8,38 @@ import useMovie from '@/hook/useMovie';
 
 interface InfoModalProps {
   visible?: boolean;
-  onClose: any;
+  onClose: () => void;
 }
 
-const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
-  const [isVisible, setIsVisible] = useState<boolean>(!!visible);
+const InfoModal: React.FC<InfoModalProps> = ({ visible = false, onClose }) => {
+  const [isVisible, setIsVisible] = useState<boolean>(visible);
 
   const { movieId } = useInfoModalStore();
-  const { data = {} } = useMovie(movieId);
+  const { data } = useMovie(movieId);
 
   useEffect(() => {
-    setIsVisible(!!visible);
+    setIsVisible(visible);
   }, [visible]);
 
-  const handleClose = useCallback(() => {
-    setIsVisible(false);
-    setTimeout(() => {
-      onClose();
-    }, 300);
-  }, [onClose]);
-
-  if (!visible) {
+  if (!isVisible) {
     return null;
   }
 
   return (
-    <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
-      <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
-        <div className={`${isVisible ? 'scale-100' : 'scale-0'} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto bg-black bg-opacity-80 transition duration-300">
+      <div className="w-auto max-w-3xl mx-auto overflow-hidden rounded-md relative">
+        <div className={`relative flex-auto transform duration-300 bg-zinc-900 drop-shadow-md ${isVisible ? 'scale-100' : 'scale-0'}`}>
 
           <div className="relative h-96">
-            <video poster={data?.thumbnailUrl} autoPlay muted loop src={data?.videoUrl} className="w-full brightness-[60%] object-cover h-full" />
-            <div onClick={handleClose} className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center">
-              <XMarkIcon className="text-white w-6" />
-            </div>
-            <div className="absolute bottom-[10%] left-10">
-              <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
+            <video poster={data?.thumbnailUrl} autoPlay muted loop src={data?.videoUrl} className="object-cover w-full h-full brightness-[60%]" />
+            <button onClick={onClose} className="absolute top-3 right-3 flex items-center justify-center w-10 h-10 rounded-full bg-black bg-opacity-70">
+              <XMarkIcon className="w-6 text-white" />
+            </button>
+            <div className="absolute left-10 bottom-[10%]">
+              <p className="text-3xl font-bold text-white md:text-4xl lg:text-5xl mb-8">
                 {data?.title}
               </p>
-              <div className="flex flex-row gap-4 items-center">
+              <div className="flex items-center gap-4">
                 <PlayButton movieId={data?.id} />
                 <FavoriteButton movieId={data?.id} />
               </div>
@@ -54,18 +47,18 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
           </div>
 
           <div className="px-12 py-8">
-            <div className="flex flex-row items-center gap-2 mb-8">
-              <p className="text-green-400 font-semibold text-lg">
+            <div className="flex items-center gap-2 mb-8">
+              <p className="text-lg font-semibold text-green-400">
                 New
               </p>
-              <p className="text-white text-lg">
+              <p className="text-lg text-white">
                 {data?.duration}
               </p>
-              <p className="text-white text-lg">
+              <p className="text-lg text-white">
                 {data?.genre}
               </p>
             </div>
-            <p className="text-white text-lg">
+            <p className="text-lg text-white">
               {data?.description}
             </p>
           </div>
@@ -74,6 +67,6 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
       </div>
     </div>
   );
-}
+};
 
 export default InfoModal;
