9515e3459afa81272f84030a5d870c03545d5bd3
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-16 11:57:24 -0500

    Refactor API handler for improved error handling and code organization

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,28 +1,27 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 
 import prismadb from '@/lib/prismadb';
-import { getSession } from "next-auth/react";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
 
-const serverAuth = async(req: NextApiRequest)=>{
-  const session = await getSession({ req});
-
-  if (!session?.user?.email){
+  if (!session?.user?.email) {
     throw new Error('Not signed in');
   }
 
   const currentUser = await prismadb.user.findUnique({
-    where:{
+    where: {
       email: session.user.email,
     }
   });
-
-  if (!currentUser){
-    throw new Error('Not SignedIn');
+  
+  if (!currentUser) {
+    throw new Error('Not signed in');
   }
 
-  return {currentUser};
-
-};
+  return { currentUser };
+}
 
 export default serverAuth;
