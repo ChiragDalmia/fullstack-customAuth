3f79be5a710d641f3e2401e7856d88c38100b48e
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-15 23:30:57 -0500

    Set up index for navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { NextPageContext } from "next";
-import useCurrentUser from "@/hook/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
-
-const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -23,14 +21,12 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
-  const {data: user} = useCurrentUser();
 
   
   return (
     <>
-    <h1>VOOODJD</h1>
-    <p className="text-white">Logged in as: {user?.email}</p>
-    <button className="h-10 bg-white" onClick={()=>signOut()}>Logout</button>
+    <Navbar />
+    
     </>
   );
 }
