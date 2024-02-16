6cd594a060d531eebdb38c3c0bedf694e2a02a5d
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-16 14:59:18 -0500

    Improved useBillboard hook with additional state management.

--- hook/useBillboard.ts
+++ hook/useBillboard.ts
@@ -1,17 +1,27 @@
 import useSwr from 'swr'
 import fetcher from '@/lib/fetcher';
+import { useMemo } from 'react';
 
-const useBillboard = () => {
-  const { data, error, isLoading } = useSwr('/api/random', fetcher, { 
+const useBillboard = (config = {}) => {
+  const { data, error, mutate, isValidating } = useSwr('/api/random', fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
-   });
-  return {
+    ...config,
+  });
+
+  const refresh = () => mutate();
+
+  const state = useMemo(() => ({
     data,
     error,
-    isLoading
-  }
+    isLoading: !error && !data,
+    isFetching: isValidating,
+    isSuccess: !error && !!data,
+    refresh,
+  }), [data, error, isValidating, mutate]);
+
+  return state;
 };
 
 export default useBillboard;
