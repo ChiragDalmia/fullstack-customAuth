2456f3ae0cacf045769abe6a1b2363e6abda11e3
Chirag Dalmia <chiragdalmia11@gmail.com>
2024-02-16 14:55:17 -0500

    Updated MobileMenu for improved maintainability.

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -4,35 +4,31 @@ interface MobileMenuProps {
   visible?: boolean;
 }
 
-const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
+const menuItems = [
+  "Home",
+  "Series",
+  "Films",
+  "New & Popular",
+  "My List",
+  "Browse by Languages",
+];
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ visible = false }) => {
   if (!visible) {
     return null;
   }
 
   return (
-    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 rounded-md flex">
+    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 rounded-md flex transition-opacity duration-1000 ease-in-out" role="menu">
       <div className="flex flex-col gap-4">
-        <div className="px-3 text-center text-white hover:underline">
-          Home
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Series
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Films
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          New & Popular
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          My List
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Browse by Languages
-        </div>
+        {menuItems.map((item, index) => (
+          <div key={index} className="px-3 text-center text-white hover:underline" role="menuitem">
+            {item}
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MobileMenu;
